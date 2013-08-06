90dc2d351a1785e564a14e4e370f2e9cd3614123
Kevin Lamping <klamping@gmail.com>
2013-08-06 16:39:02 -0500

    added simple script which does basic pull of commit history

---
+++ getGitCommits.js
@@ -0,0 +1,20 @@
+var https = require('https');
+
+var options = {
+    host: 'github.com',
+    path: '/users/klamping/contributions_calendar_data'
+}
+var request = https.request(options, function (res) {
+    var data = '';
+    res.on('data', function (chunk) {
+        data += chunk;
+    });
+    res.on('end', function () {
+        console.log(data);
+
+    });
+});
+request.on('error', function (e) {
+    console.log(e.message);
+});
+request.end();
